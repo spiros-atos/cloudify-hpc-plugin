8fa665bfb4c25412b832f6427ab69fd10ebbb337
spiros-atos <spiros.michalakopoulos.external@atos.net>
2018-11-14 10:21:57 +0100

    a

--- hpc_plugin/tasks.py
+++ hpc_plugin/tasks.py
@@ -12,9 +12,6 @@
 # WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 # See the License for the specific language governing permissions and
 # limitations under the License.
-
-import pdb
-
 """ Holds the plugin tasks """
 
 import traceback
@@ -98,8 +95,7 @@ def configure_execution(
             ctx.logger.info('L95')
 
         try:
-            pdb.set_trace()
-            ctx.logger.info('L98')
+            ctx.logger.info('L98' + str(credentials))
             client = SshClient(credentials, ctx.logger)
             ctx.logger.info('L100')
         except Exception as exp:
