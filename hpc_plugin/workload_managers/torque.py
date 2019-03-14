710fc3eb636d33ca370b9f10b0816d1ae528f51a
spiros.michalakopoulos@atos.net <spiros.michalakopoulos@atos.net>
2019-03-14 09:41:59 +0100

    logs

--- hpc_plugin/workload_managers/torque.py
+++ hpc_plugin/workload_managers/torque.py
@@ -209,7 +209,7 @@ class Torque(WorkloadManager):
     @staticmethod
     def _get_states_detailed(workdir, credentials, job_names, logger):
 
-        ctx.logger.info('TORQUE.PY L212')
+        logger.info('TORQUE.PY L212')
 
         """
         Get job states by job names
