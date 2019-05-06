0f8589807bcc35003581bbbfc803cda45c3e664f
spiros.michalakopoulos@atos.net <spiros.michalakopoulos@atos.net>
2019-05-06 08:24:56 +0200

    k8s tests take 1

--- hpc_plugin/tests/workflow_tests.py
+++ hpc_plugin/tests/workflow_tests.py
@@ -426,5 +426,40 @@ class TestPlugin(unittest.TestCase):
             logging.warning('[WARNING] Login could not be tested')
 
 
+# Kubernetes (K8s)
+    @workflow_test(os.path.join('blueprints', 'blueprint_k8s.yaml'),
+                   copy_plugin_yaml=True,
+                   resources_to_copy=[(os.path.join('blueprints',
+                                                    'inputs_def.yaml'),
+                                       './'),
+                                      # (os.path.join('blueprints', 'scripts',
+                                      #               'bootstrap_' +
+                                      #               'sbatch_example.sh'),
+                                      #  'scripts'),
+                                      # (os.path.join('blueprints', 'scripts',
+                                      #               'revert_' +
+                                      #               'sbatch_example.sh'),
+                                      #  'scripts')
+                                      ],
+                   inputs='set_inputs')
+    def test_k8s(self, cfy_local):
+        """ Single SBATCH Job Blueprint """
+        cfy_local.execute('install', task_retries=0)
+        # cfy_local.execute('run_jobs', task_retries=0)
+        cfy_local.execute('uninstall', task_retries=0)
+
+        # extract single node instance
+        instance = cfy_local.storage.get_node_instances()[0]
+
+        # due to a cfy bug sometimes login keyword is not ready in the tests
+        if 'login' in instance.runtime_properties:
+            # assert runtime properties is properly set in node instance
+            self.assertEqual(instance.runtime_properties['login'],
+                             True)
+        else:
+            logging.warning('[WARNING] Login could not be tested')
+
+
+
 if __name__ == '__main__':
     unittest.main()
